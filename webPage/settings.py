68ba39f195c90acb8087c1de760a3b5888775c64
paula andrea betancur <paulabetan1@gmail.com>
2024-01-02 13:55:40 -0500

    Configuration web

--- webPage/settings.py
+++ webPage/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-if_4dtgjixs4s-$rumjswmh9msl##7m85e1hcfs8yb8+of3ll+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
