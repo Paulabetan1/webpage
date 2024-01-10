fa73344c1c4feafb6f964f504d0eca7fa3a6aa4f
paula andrea betancur <paulabetan1@gmail.com>
2024-01-09 20:56:23 -0500

    fix errors

--- webPage/settings.py
+++ webPage/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-if_4dtgjixs4s-$rumjswmh9msl##7m85e1hcfs8yb8+of3ll+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 
