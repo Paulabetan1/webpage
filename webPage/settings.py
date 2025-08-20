2d9e3cb1e1f609c2201de353c795262ffb9ce166
Paulabetan1 <76755884+Paulabetan1@users.noreply.github.com>
2025-08-19 18:36:10 -0600

    Update settings.py

--- webPage/settings.py
+++ webPage/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-if_4dtgjixs4s-$rumjswmh9msl##7m85e1hcfs8yb8+of3ll+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','website-app.herokuapp.com','www.sauzesmobiliario.co', 'www.sauzesmobiliario.com']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
